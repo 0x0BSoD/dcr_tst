f1f8156800c6c4e000fadab67ac9c8dc2797eb8c
Alexander Simonov <zlodey23@gmail.com>
2018-07-06 18:32:16 +0300

    To gitlab

--- app/identidock.py
+++ app/identidock.py
@@ -1,10 +1,28 @@
 from flask import Flask
 
 app = Flask(__name__)
+default_name = 'Jon Doe'
+
+@app.route('/')
+def index():
+    name = default_name
+
+    header = '<html><head><title>Idemtidock</title></head><body>'
+    body = '''<form metho="POST">
+              Hello <input type="text" name="name" value="{}">
+              <input type="submit" value="submit">
+              </form>
+              <p>You look like a:
+              <img src="/monster/monster.png"/>
+              '''.format(name)
+    footer = '</body></html>'
+
+    return header + body + footer
+
 
 @app.route('/ping')
-def index():
-    return "PONG"
+def pingpong():
+    return "pong"
 
 
 if __name__ == '__main__':
