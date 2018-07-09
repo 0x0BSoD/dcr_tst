598debe490bbdd2018d3c5d311af7b9e6ca610e5
Aleksandr Simonov <zlodey23@gmail.com>
2018-07-09 10:50:56 +0300

    Add compose for jenkins

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
     echo "Run Dev Server"
     exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Run Dev Tests"
+    exec python "tests.py"   
 else
     echo "Run Prod Server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
